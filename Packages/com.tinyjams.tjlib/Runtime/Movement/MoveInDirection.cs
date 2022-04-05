b31ba1d3df9edf4ca15ad0019479f47c074e166a
Jan Mikusch <jmikusch.mmt-m2020@fh-salzburg.ac.at>
2022-04-05 17:24:39 +0200

    Add Functions to lib

---
+++ Packages/com.tinyjams.tjlib/Runtime/Movement/MoveInDirection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace com.tinyjams.tjlib.Runtime.Movement
+{
+    public class MoveInDirection : MonoBehaviour
+    {
+        [SerializeField] private Vector3 direction;
+        [SerializeField] private float speed;
+
+        private void Awake()
+        {
+            this.direction.Normalize();
+        }
+
+        private void Update()
+        {
+            var translation = this.direction * this.speed * Time.deltaTime;
+            this.transform.Translate(translation);
+        }
+    }
+}
