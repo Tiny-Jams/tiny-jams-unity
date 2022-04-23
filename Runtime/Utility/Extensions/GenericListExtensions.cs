fd4a4d5f9d1b601616e25bfb3933120b61d779d0
Jan Mikusch <jmikusch.mmt-m2020@fh-salzburg.ac.at>
2022-04-23 17:17:13 +0200

    Add GetRandomListElement ExtensionMethod

---
+++ Runtime/Utility/Extensions/GenericListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+namespace com.tinyjams.tjlib.Runtime.Utility.Extensions
+{
+    public static class GenericListExtensions
+    {
+        public static T GetRandomElement<T>(this List<T> list)
+        {
+            var idx = Random.Range(0, list.Count);
+            return list[idx];
+        }
+    }
+}
